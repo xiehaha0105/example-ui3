e6f3d80e53c4bb841ddcab1f792721d7ac406b3d
xiehaha0105 <xhaha0105@163.com>
2024-06-27 03:13:48 +0000

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 80,//端口号
+    port: 9000,//端口号
     host: true,//ip地址 或 '0.0.0.0' 或 "loaclhost"
     open: true, //启动后是否自动打开浏览器
   },
